3341daebbbff384fddc0c0e63e3f283993ff64a0
Aman David <amandavid0032@gmail.com>
2023-08-23 15:37:17 +0530

    proejct is done to work

---
+++ script.js
@@ -0,0 +1,500 @@
+// read more
+// Audi
+function Audi2() {
+  var dots = document.getElementById("Audi1");
+  var moreText = document.getElementById("Audi");
+  var btnText = document.getElementById("Audi3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+// Mercedes
+function Mercedes2() {
+  var dots = document.getElementById("Mercedes1");
+  var moreText = document.getElementById("Mercedes");
+  var btnText = document.getElementById("Mercedes3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+// Lambo
+function Lambo2() {
+  var dots = document.getElementById("Lambo1");
+  var moreText = document.getElementById("Lambo");
+  var btnText = document.getElementById("Lambo3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+// Ferrari
+function Ferrari2() {
+  var dots = document.getElementById("Ferrari1");
+  var moreText = document.getElementById("Ferrari");
+  var btnText = document.getElementById("Ferrari");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+// Rolls
+function Rolls2() {
+  var dots = document.getElementById("Rolls1");
+  var moreText = document.getElementById("Rolls");
+  var btnText = document.getElementById("Rolls3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+// Bentley
+function Bentley2() {
+  var dots = document.getElementById("Bentley1");
+  var moreText = document.getElementById("Bentley");
+  var btnText = document.getElementById("Bentley3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+// Bmw
+function Bmw2() {
+  var dots = document.getElementById("Bmw1");
+  var moreText = document.getElementById("Bmw");
+  var btnText = document.getElementById("Bmw3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+// Bugatti
+function Bugatti2() {
+  var dots = document.getElementById("Bugatti1");
+  var moreText = document.getElementById("Bugatti");
+  var btnText = document.getElementById("Bugatti3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+// MAserati
+function Maserati2() {
+  var dots = document.getElementById("Maserati1");
+  var moreText = document.getElementById("Maserati");
+  var btnText = document.getElementById("Maserati3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+// Fortuner
+function Fortuner2() {
+  var dots = document.getElementById("Fortuner1");
+  var moreText = document.getElementById("Fortuner");
+  var btnText = document.getElementById("Fortuner3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+//Land
+function Land2() {
+  var dots = document.getElementById("Land1");
+  var moreText = document.getElementById("Land");
+  var btnText = document.getElementById("Land3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+//Ford
+function Ford2() {
+  var dots = document.getElementById("Ford1");
+  var moreText = document.getElementById("Ford");
+  var btnText = document.getElementById("Ford3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+//Mahindra
+function Mahindra2() {
+  var dots = document.getElementById("Mahindra1");
+  var moreText = document.getElementById("Mahindra");
+  var btnText = document.getElementById("Mahindra3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+//Nissan
+function Nissan2() {
+  var dots = document.getElementById("Nissan1");
+  var moreText = document.getElementById("Nissan");
+  var btnText = document.getElementById("Nissan3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+//Mini
+function Mini2() {
+  var dots = document.getElementById("Mini1");
+  var moreText = document.getElementById("Mini");
+  var btnText = document.getElementById("Mini3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+//Verna
+function Verna2() {
+  var dots = document.getElementById("Verna1");
+  var moreText = document.getElementById("Verna");
+  var btnText = document.getElementById("Verna3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+//Skoda
+function Skoda2() {
+  var dots = document.getElementById("Skoda1");
+  var moreText = document.getElementById("Skoda");
+  var btnText = document.getElementById("Skoda3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+//Scorpio
+function Scorpio2() {
+  var dots = document.getElementById("Scorpio1");
+  var moreText = document.getElementById("Scorpio");
+  var btnText = document.getElementById("Scorpio3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+
+// contact 
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// help
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
+
+localStorage.setItem("data", JSON.stringify({
+  loginDetails: [{
+    email: "amandavid@gmail.com", password: "1234",
+  }]
+}))
+
+
+
+
+
+var productName = document.getElementById('productName');
+var productDesc = document.getElementById('productDesc');
+var productModel = document.getElementById('productModel');
+var productPrice = document.getElementById('productPrice');
+var productBtn = document.getElementById('productBtn');
+var productImg = document.getElementById('productImg');
+var formControl = document.getElementsByClassName('form-control');
+var myClose = document.getElementsByClassName('myClose');
+var mySearchInp = document.getElementById('mySearchInp');
+var productHeadline = document.getElementById('productHeadline');
+//var viewBtn = document.getElementById('viewBtn');
+var successProductAlert = document.getElementById('successProductAlert');
+var dataRow = document.getElementById('dataRow');
+var productContainer;
+var currentIndex = 0;
+//check for local storage at begining
+if (localStorage.getItem('productsStorage') == null) {
+  productContainer = [];
+  productHeadline.style.display = 'none';
+  searchItem.style.display = 'none';
+} else {
+  productContainer = JSON.parse(localStorage.getItem('productsStorage'));
+  showProducts();
+}
+//check for empty fields to disable button
+disableBtn();
+
+productBtn.addEventListener('click', function () {
+  if (productBtn.innerHTML == 'add product') {
+    //disableBtn();
+    addProducts();
+    showAlert();
+    showProducts();
+    emptyFields();
+  } else {
+    saveUpdate();
+    emptyFields();
+  }
+
+});
+
+function showAlert() {
+  $(successProductAlert).fadeIn(500, function () {
+    $(successProductAlert).fadeOut(2000);
+  });
+}
+//if (productName.value == '' && productDesc.value == '' && productModel.value == '' && productPrice.value == '')
+function disableBtn() {
+  for (var i = 0; i < formControl.length - 1; i++) {
+    if (formControl[i].value == '') {
+      productBtn.disabled = true;
+      //console.log('empty');
+    } else {
+      productBtn.removeAttribute('disabled');
+    }
+  }
+}
+
+function addProducts() {
+
+  var products = {
+    productName: productName.value,
+    // productImg: productImg.value,
+    productDesc: productDesc.value,
+    productModel: productModel.value,
+    productPrice: productPrice.value
+  };
+  productContainer.push(products);
+
+  localStorage.setItem('productsStorage', JSON.stringify(productContainer));
+  console.log('products added');
+}
+
+function showProducts() {
+
+  var rows = '';
+  for (var i = 0; i < productContainer.length; i++) {
+    rows += '<div class="col-lg-4 col-md-6 col-sm-12 my-2 products"><div class="product"><div class="card p-1 text-center m-auto" style="width: 18rem;"><div class="d-flex justify-content-between"><i class="fas fa-edit fa-2x" onclick="updateProduct(' + i + ')"></i><i class="fa fa-times-circle fa-2x " onclick="deleteItem(' + i + ')"></i></div><img class="img-fluid" src="images/3191.jpg" class="card-img-top" alt="test"><div class="card-body"><h5 class="card-title">' + productContainer[i].productName + '</h5><p class="card-text">' + productContainer[i].productDesc + '</p><a href="#" class="btn btn-primary">' + productContainer[i].productPrice + ' L.E</a></div></div></div></div>';
+  }
+  document.getElementById('dataRow').innerHTML = rows;
+  //console.log('show products func');
+  productContainer = JSON.parse(localStorage.getItem('productsStorage'));
+  productHeadline.style.display = 'block';
+  searchItem.style.display = 'block';
+}
+
+function updateProduct(index) {
+  //console.log('clicked');
+  productName.value = productContainer[index].productName;
+  productDesc.value = productContainer[index].productDesc;
+  productPrice.value = productContainer[index].productPrice;
+  productModel.value = 'sn123';
+  productBtn.innerHTML = 'update product';
+  currentIndex = index;
+}
+
+function saveUpdate() {
+  var products = {
+    productName: productName.value,
+    productDesc: productDesc.value,
+    productModel: productModel.value,
+    productPrice: productPrice.value
+  };
+  productContainer[currentIndex] = products;
+  localStorage.setItem('productsStorage', JSON.stringify(productContainer));
+  showProducts();
+  productBtn.innerHTML = 'add product';
+}
+
+function emptyFields() {
+  for (var i = 0; i < formControl.length; i++) {
+    formControl[i].value = '';
+  }
+  disableBtn();
+}
+
+function deleteItem(item) {
+  productContainer.splice(item, 1);
+  localStorage.setItem('productsStorage', JSON.stringify(productContainer));
+  showProducts();
+}
+mySearchInp.addEventListener('keyup', function (e) {
+  //console.log(e.target.value);
+  var rows = '';
+  for (var i = 0; i < productContainer.length; i++) {
+    if (productContainer[i].productName.toLowerCase().includes(e.target.value.toLowerCase())) {
+      rows += '<div class="col-lg-4 col-md-6 col-sm-12 my-2 products"><div class="product"><div class="card p-1 text-center m-auto" style="width: 18rem;"><div class="d-flex justify-content-between"><i class="fas fa-edit fa-2x" onclick="updateProduct(' + i + ')"></i><i class="fa fa-times-circle fa-2x" onclick="deleteItem(' + i + ')"></i></div><img class="img-fluid" src="images/3191.jpg" class="card-img-top" alt="test"><div class="card-body"><h5 class="card-title">' + productContainer[i].productName + '</h5><p class="card-text">' + productContainer[i].productDesc + '</p><a href="#" class="btn btn-primary">' + productContainer[i].productPrice + '</a></div></div></div></div>';
+    }
+  }
+  document.getElementById('dataRow').innerHTML = rows;
+});
+
+//nav bar scroll
+let navHeight = $('nav').outerHeight();
+//console.log('nav height = ' + navHeight);
+$(window).scroll(function () {
+  let scrollTop = $(window).scrollTop();
+  if (scrollTop > navHeight) {
+    document.querySelector('nav').style.position = 'fixed';
+    document.querySelector('nav').style.zIndex = '1000';
+    document.querySelector('.my-Container').style.width = '85%';
+    document.querySelector('nav').style.width = '85%';
+    document.querySelector('.marginTop').style.marginTop = '0rem';
+  } else {
+    // $('nav').css('backgroundColor', 'green');
+    document.querySelector('nav').style.position = 'relative';
+    document.querySelector('.my-Container').style.width = '85%';
+    document.querySelector('nav').style.width = '100%';
+    document.querySelector('.marginTop').style.marginTop = '3rem';
+  }
+});
+
+
+
